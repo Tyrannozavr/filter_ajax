e8f027c4ddf99ac9800d3fd71cf055220f920c1c
dmiv <dmitriy40647274@gmail.com>
2022-04-17 17:39:14 +0300

    I will change raw to extra

--- filter/models.py
+++ filter/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Table(models.Model):
+    date = models.DateField()
     title = models.CharField(max_length=255)
+    count = models.IntegerField()
+    distance = models.IntegerField()
 
+    def __str__(self):
+        return self.title
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
--- filter/views.py
+++ filter/views.py
@@ -11,15 +11,43 @@ def index(request):
 
 
 class JsonFilterMoviesView(ListView):
+    model = Table
+    paginate_by = 5
     """Фильтр фильмов в json"""
     def get_queryset(self):
-        # queryset = Movie.objects.filter(
-        #     Q(year__in=self.request.GET.getlist("year")) |
-        #     Q(genres__in=self.request.GET.getlist("genre"))
-        # ).distinct().values("title", "tagline", "url", "poster")
+        fields = {'1': 'title', '2': 'count', '3': 'distance'}
+        column = fields.get(self.request.GET.get('title'))
+        condition = self.request.GET.get('count')
+        value = self.request.GET.get('value')
+        queryset = Table.objects.all()
+        if condition == '1':
+            queryset = Table.objects.raw(f'''
+                SELECT * FROM table_table
+                WHERE {column} > {value};
+                ''').all
+        elif condition == '2':
+
+            queryset = Table.objects.raw(f'''-- 
+        --                 SELECT * FROM table_table
+        --         #         WHERE {column} < {value};
+                        ''')
+        elif condition == '3':
+            f = Table.objects.raw(f'''
+                        SELECT * FROM table_table
+                        WHERE {column} = {value};
+                        ''')
+        elif condition == '4':
+            queryset = Table.objects.raw(f'''
+                    SELECT * FROM table_table
+                    WHERE {column} LIKE "%{value}%";''')
+        print(queryset)
         queryset = Table.objects.all().distinct().values("title")
+        print(queryset)
         return queryset
+        # return queryset.distinct().values('title')
 
     def get(self, request, *args, **kwargs):
         queryset = list(self.get_queryset())
         return JsonResponse({"movies": queryset}, safe=False)
+
+
